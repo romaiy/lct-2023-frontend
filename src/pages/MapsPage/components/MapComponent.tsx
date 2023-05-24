a290ae6e757f1a0bdb82fe9415386071640b60c6
romai <sokolovgit256@gmail.com>
2023-05-25 01:26:07 +0600

    WorkingMaps commit

--- src/pages/MapsPage/components/MapComponent.tsx
+++ src/pages/MapsPage/components/MapComponent.tsx
@@ -8,32 +8,27 @@ const MapComponent = () => {
     const [markers, setMarkers] = useState <{x: number, y: number}[]>([]);
 
     useEffect(() => {
-        let Point = {pos: '37.587614 55.753083'}
-        location.state.addresses.map((item: string) => {
-
-            let stringArray: string[] = Point.pos.split(' ');
-            let numberArray: number[] = [];
-            for (let i = 0; i < 2; i++) {
-                numberArray.push(+stringArray[i]);
-            };
-            setMarkers([...markers!, {x: numberArray[0], y: numberArray[1]}]);
-        });
-    }, [])
-
-    const handleGet = async () => {
-        try {
-            const response = await axios.get('https://geocode-maps.yandex.ru/1.x/?apikey=5fff5614-b0c5-4970-b75d-28aa88c46171&format=json&geocode=Москва, улица Тверская, дом 6')
-            console.log(response.data)
-        } catch (error) {
-            
+        for (let i = 0; i < location.state.addresses.length; i++) {
+            try {
+                axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=5fff5614-b0c5-4970-b75d-28aa88c46171&format=json&geocode=Москва, ${location.state.addresses[i]}`)
+                .then(response => {
+                    let stringArray: string[] = response.data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ');
+                    let numberArray: number[] = [];
+                    for (let j = 0; j < 2; j++) {
+                        numberArray.push(+stringArray[j]);
+                    };
+                    console.log(response.data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos)
+                    setMarkers(item => [...item!, {x: numberArray[1], y: numberArray[0]}]);
+                })
+            } catch (error) {
+                console.log(error)
+            }
         }
-    }
+    }, [location])
 
     return (
     <div>
-        {markers && markers.map(item => (
-            <div>{item.x} {item.y}</div>
-        ))}
+        {markers ? 
         <Map
             defaultState={{
                 center: [55.75, 37.57],
@@ -43,9 +38,11 @@ const MapComponent = () => {
             modules={["control.ZoomControl", "control.FullscreenControl"]}
             width={1139} height={'100vh'}
         >
-            <Placemark defaultGeometry={[55.75, 37.57]} />
-            <Placemark defaultGeometry={[55.77, 37.57]} />
+            {markers.map((item, index) => (
+                <Placemark key={index} defaultGeometry={[item.x, item.y]}/>
+            ))}
         </Map>
+        : <></>}
     </div>
 
     );
